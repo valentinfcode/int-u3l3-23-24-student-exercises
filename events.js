7b0479304a25b15eede236167139f782bb68444d
valentinfcode <147728698+valentinfcode@users.noreply.github.com>
2024-01-18 14:49:46 +0000

    Finished all Tasks

--- events.js
+++ events.js
@@ -11,56 +11,39 @@ let endGameButton = document.querySelector(".end-game");
 let updateScoreButton = document.querySelector(".update-score");
 let growButton = document.querySelector(".grow");
 
-/* 1. When jump button is clicked, Mario jumps and coin disappears.  
+// 1. When jump button is clicked, Mario jumps and coin disappears.  
 
-________.addEventListener("click", function() {
+jumpButton.addEventListener("click", function() {
     mario.style.bottom = "120px";
     coin.style.display = "none";
 });
 
-*/
+// 2. When jump down button is clicked, Mario jumps down. 
 
-
-/* 2. When jump down button is clicked, Mario jumps down. 
-
-________.addEventListener("click", function() {
+jumpDownButton.addEventListener("click", function() {
     mario.style = "bottom: 35px";
 });
 
-*/
+// 3. When change color button is clicked, background color changes. 
 
-
-/* 3. When change color button is clicked, background color changes. 
-
-________.addEventListener("click", function() {
-    background.style.backgroundColor = "springGreen"; // you can change the color to something different
+changeColorButton.addEventListener("click", function() {
+    background.style.backgroundColor = "green"; // you can change the color to something different
 });
 
-*/
+// 4. When end game button is clicked, the title of the page says game over. 
 
-
-/* 4. When end game button is clicked, the title of the page says game over. 
-
-________._______ ("___", ________() {
+endGameButton.addEventListener ("click", function() {
     title.innerHTML = "Game over.";
 });
 
-*/
+// 5. When update score button is clicked, the counter score becomes "1" instead of 0. 
 
-
-/* 5. When update score button is clicked, the counter score becomes "1" instead of 0. 
-
-________._______ ("___", ________() {
+updateScoreButton.addEventListener ("click", function() {
     score.innerHTML = 1;
 });
 
-*/
+// 6: When clicking the "Grow" button the Mario image gets bigger. Write the whole click handler! 
 
-
-/* 6: When clicking the "Grow" button the Mario image gets bigger. Write the whole click handler! 
-
-{
+growButton.addEventListener ("click", function() {
     mario.style.width = "200px";
-}
-
-*/
+});
